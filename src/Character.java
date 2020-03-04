c1371ef8b54700c1821b19288656310600e4b5a5
hendrmat <47726101+hendrmat@users.noreply.github.com>
2020-03-04 15:16:26 -0500

    First commit of mine

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+public class Character {
+    //Lists the types of classes available for selection
+    enum primaryClass {Fighter, Thief, Cleric, Wizard}
+    //Lists the statistic categories for a character
+    int strength;
+    int dexterity;
+    int constitution;
+    int intelligence;
+
+}
